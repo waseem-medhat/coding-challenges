6c82d658a364a09e841f42551d1850514421501d
waseem-medhat <wipdev.tech@gmail.com>
2025-08-31 22:31:33 +0300

    refactor arg parsing with result; count chars

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,27 @@ enum Count {
 }
 
 impl Config {
-    fn from_args() -> Self {
+    fn from_args() -> Result<Self, String> {
         let args: Vec<String> = env::args().collect();
         match args.len() {
-            2 => Self {
+            2 => Result::Ok(Self {
                 filename: args[1].clone(),
                 count: Count::All,
-            },
+            }),
             3 => {
                 let count = match args[1].as_str() {
                     "-c" => Count::Bytes,
                     "-m" => Count::Chars,
                     "-w" => Count::Words,
                     "-l" => Count::Lines,
-                    _ => panic!("invalid arg"),
+                    _ => return Err(String::from("invalid arg")),
                 };
-                Self {
+                Result::Ok(Self {
                     filename: args[2].clone(),
                     count,
-                }
+                })
             }
-            _ => panic!("invalid number of args; usage: wc [option] <filename>"),
+            _ => Err(String::from("invalid number of args")),
         }
     }
 }
@@ -45,12 +45,17 @@ fn count(content: String, count: Count) -> usize {
         Count::Bytes => content.len(),
         Count::Lines => content.chars().filter(|&c| c == '\n').count(),
         Count::Words => content.split_ascii_whitespace().count(),
+        Count::Chars => content.chars().count(),
         _ => panic!("TODO :D"),
     }
 }
 
 fn main() {
-    let config = Config::from_args();
+    let config = match Config::from_args() {
+        Err(msg) => panic!("{msg}; usage: wc [option] <filename>"),
+        Ok(config) => config,
+    };
+
     let file_content = match fs::read_to_string(config.filename) {
         Err(_) => panic!("couldn't read file!"),
         Ok(str) => str,
