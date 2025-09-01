bb831e362fa80ef0122f40092411dcc9445ac1cc
waseem-medhat <wipdev.tech@gmail.com>
2025-09-01 03:52:21 +0300

    add counting from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::env;
 use std::fs;
+use std::io;
+use std::io::IsTerminal;
+use std::io::Read;
 
 struct Config {
-    filename: String,
+    input_kind: InputKind,
     count_opt: CountOpt,
+}
+
+enum InputKind {
+    File(String),
+    Stdin,
 }
 
 enum CountOpt {
@@ -16,32 +24,50 @@ enum CountOpt {
 
 impl Config {
     fn from_args() -> Result<Self, String> {
+        let has_stdin = !io::stdin().is_terminal();
         let args: Vec<String> = env::args().collect();
-        match args.len() {
-            2 => Result::Ok(Self {
-                filename: args[1].clone(),
-                count_opt: CountOpt::All,
-            }),
-            3 => {
-                let count = match args[1].as_str() {
-                    "-c" => CountOpt::Bytes,
-                    "-m" => CountOpt::Chars,
-                    "-w" => CountOpt::Words,
-                    "-l" => CountOpt::Lines,
-                    _ => return Err(String::from("invalid arg")),
-                };
-                Result::Ok(Self {
-                    filename: args[2].clone(),
-                    count_opt: count,
-                })
-            }
-            _ => Err(String::from("invalid number of args")),
-        }
+
+        let (input_kind, count_opt) = match args.len() {
+            // input | wc
+            1 if has_stdin => (InputKind::Stdin, CountOpt::All),
+
+            // wc <filename>
+            2 if !has_stdin => (InputKind::File(args[1].clone()), CountOpt::All),
+
+            // input | wc <flag>
+            2 => match arg_to_opt(&args[1]) {
+                Ok(opt) => (InputKind::Stdin, opt),
+                Err(msg) => return Err(msg),
+            },
+
+            //  wc <flag> <filename>
+            3 => match arg_to_opt(&args[1]) {
+                Ok(opt) => (InputKind::File(args[2].clone()), opt),
+                Err(msg) => return Err(msg),
+            },
+
+            _ => return Err(String::from("invalid number of args")),
+        };
+
+        Ok(Config {
+            input_kind,
+            count_opt,
+        })
+    }
+}
+
+fn arg_to_opt(arg: &String) -> Result<CountOpt, String> {
+    match arg.as_str() {
+        "-c" => Ok(CountOpt::Bytes),
+        "-m" => Ok(CountOpt::Chars),
+        "-w" => Ok(CountOpt::Words),
+        "-l" => Ok(CountOpt::Lines),
+        _ => return Err(String::from("invalid arg")),
     }
 }
 
 fn cmd_count(content: &String, config: &Config) -> String {
-    let count_str = match config.count_opt {
+    match config.count_opt {
         CountOpt::Bytes => count_bytes(content).to_string(),
         CountOpt::Lines => count_lines(content).to_string(),
         CountOpt::Words => count_words(content).to_string(),
@@ -53,9 +79,7 @@ fn cmd_count(content: &String, config: &Config) -> String {
 
             format!(" {:>7} {:>7} {:>7}", lines, words, bytes)
         }
-    };
-
-    format!("{count_str} {}", config.filename)
+    }
 }
 
 fn count_bytes(content: &String) -> usize {
@@ -80,11 +104,23 @@ fn main() {
         Ok(config) => config,
     };
 
-    let file_content = match fs::read_to_string(&config.filename) {
-        Err(_) => panic!("couldn't read file!"),
-        Ok(str) => str,
+    let content = match config.input_kind {
+        InputKind::File(ref filename) => match fs::read_to_string(filename) {
+            Err(err) => panic!("couldn't read file {filename}: {:?}", err),
+            Ok(str) => str,
+        },
+        InputKind::Stdin => {
+            let mut stdin_string = String::new();
+            io::stdin()
+                .read_to_string(&mut stdin_string)
+                .expect("couldn't read stdin");
+            stdin_string
+        }
     };
 
-    let file_count = cmd_count(&file_content, &config);
-    println!("{file_count}");
+    let count = cmd_count(&content, &config);
+    match config.input_kind {
+        InputKind::File(filename) => println!("{count} {filename}"),
+        InputKind::Stdin => println!("{count}"),
+    }
 }
