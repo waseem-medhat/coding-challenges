97e993da187aa10426d981349634fca78281ff59
waseem-medhat <wipdev.tech@gmail.com>
2025-09-20 03:22:32 +0300

    cut: support delimiter arg

--- src/bin/cut/config.rs
+++ src/bin/cut/config.rs
@@ -5,9 +5,22 @@ use std::iter::Skip;
 pub struct Config {
     field_num: u32, // index starts at 1
     file_name: String,
+    delimiter: String,
 }
 
 impl Config {
+    pub fn from_args() -> Config {
+        let mut args = env::args().skip(1);
+        parse(
+            &mut args,
+            Config {
+                field_num: 0,
+                file_name: String::new(),
+                delimiter: String::from("\t"),
+            },
+        )
+    }
+
     pub fn field_num(&self) -> u32 {
         self.field_num
     }
@@ -16,15 +29,8 @@ impl Config {
         self.file_name.clone()
     }
 
-    pub fn from_args() -> Config {
-        let mut args = env::args().skip(1);
-        parse(
-            &mut args,
-            Config {
-                field_num: 0,
-                file_name: String::new(),
-            },
-        )
+    pub fn delimiter(&self) -> String {
+        self.delimiter.clone()
     }
 }
 
@@ -39,6 +45,19 @@ fn parse(args: &mut Skip<Args>, acc: Config) -> Config {
                 Config {
                     field_num,
                     file_name: acc.file_name(),
+                    delimiter: acc.delimiter(),
+                },
+            )
+        }
+
+        Some(arg) if arg.starts_with("-d") => {
+            let delimiter = arg[2..].to_string();
+            parse(
+                args,
+                Config {
+                    field_num: acc.field_num(),
+                    file_name: acc.file_name(),
+                    delimiter,
                 },
             )
         }
@@ -49,6 +68,7 @@ fn parse(args: &mut Skip<Args>, acc: Config) -> Config {
                 Config {
                     field_num: acc.field_num(),
                     file_name: arg,
+                    delimiter: acc.delimiter(),
                 },
             ),
             Err(_) => panic!("file {arg} doesn't exist"),
--- src/bin/cut/main.rs
+++ src/bin/cut/main.rs
@@ -6,13 +6,13 @@ use std::fs;
 fn main() {
     let config = Config::from_args();
     let file = fs::read_to_string(config.file_name()).expect("couldn't read file");
-    print_field(file, config.field_num());
+    print_field(file, config.field_num(), config.delimiter());
 }
 
-fn print_field(file: String, field_num: u32) {
+fn print_field(file: String, field_num: u32, delimiter: String) {
     file.lines().for_each(|line| {
         let field = line
-            .split("\t")
+            .split(&delimiter)
             .nth((field_num - 1).try_into().unwrap())
             .expect("unexpected end of line");
         println!("{field}");
