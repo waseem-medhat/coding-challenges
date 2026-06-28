39e1cf9fbc031f80f7b2a80ff42cde8e63d31b9b
waseem-medhat <wipdev.tech@gmail.com>
2026-06-28 12:43:12 +0300

    portscan: some cleanup

--- src/bin/portscan/config.rs
+++ src/bin/portscan/config.rs
@@ -1,17 +1,9 @@
 use anyhow::anyhow;
-use std::{
-    env,
-    net::{IpAddr, ToSocketAddrs},
-};
+use std::env;
 
 pub enum Config {
-    SinglePort(Hosts, u16),
-    Vanilla(Hosts),
-}
-
-pub enum Hosts {
-    Names(Vec<String>),
-    // Ips(Vec<IpAddr>),
+    SinglePort(Vec<String>, u16),
+    Vanilla(Vec<String>),
 }
 
 impl Config {
@@ -36,23 +28,17 @@ impl Config {
     }
 
     pub fn hosts(&self) -> Vec<String> {
-        let hosts = match self {
-            Self::SinglePort(hosts, _) => hosts,
-            Self::Vanilla(hosts) => hosts,
-        };
-
-        match hosts {
-            Hosts::Names(names) => names.clone(),
+        match self {
+            Self::SinglePort(hosts, _) => hosts.clone(),
+            Self::Vanilla(hosts) => hosts.clone(),
         }
     }
 }
 
 fn build_config(hosts_arg: Option<Vec<String>>, port_arg: Option<u16>) -> anyhow::Result<Config> {
     match (hosts_arg, port_arg) {
-        (Some(hosts), Some(port)) if hosts.len() == 1 => {
-            Ok(Config::SinglePort(Hosts::Names(hosts), port))
-        }
-        (Some(hosts), None) if hosts.len() == 1 => Ok(Config::Vanilla(Hosts::Names(hosts))),
+        (Some(hosts), Some(port)) if hosts.len() == 1 => Ok(Config::SinglePort(hosts, port)),
+        (Some(hosts), None) if hosts.len() == 1 => Ok(Config::Vanilla(hosts)),
         (None, _) => Err(anyhow!("host(s) must be provided")),
         _ => Err(anyhow!(
             "invalid inputs: currently only supporting single-host single-port scans and vanilla scans"
@@ -62,20 +48,4 @@ fn build_config(hosts_arg: Option<Vec<String>>, port_arg: Option<u16>) -> anyhow
 
 fn parse_hosts(host_str: &str) -> Vec<String> {
     host_str.split(",").map(|s| s.to_string()).collect()
-}
-
-fn parse_addr(addr_str: &str) -> anyhow::Result<IpAddr> {
-    match addr_str.parse() {
-        Ok(ip) => Ok(ip),
-        Err(_) => {
-            let host = addr_str.to_socket_addrs();
-            match host {
-                Err(_) => Err(anyhow!("cannot parse host 1 {}", addr_str)),
-                Ok(mut sock_addrs) => match sock_addrs.next() {
-                    None => Err(anyhow!("cannot parse host 2 {}", addr_str)),
-                    Some(sock_addr) => Ok(sock_addr.ip()),
-                },
-            }
-        }
-    }
 }
